2fb3aeedda6c40cd995a4aa5721d3fa9daee2f1b
fappnatics <davms812@gmail.com>
2015-03-11 00:09:10 +0100

    Usuarios conectados y adaptador de lista a medias.

--- app/src/main/java/com/cashmyapps/core/cashmyappsproject/Ranking.java
+++ app/src/main/java/com/cashmyapps/core/cashmyappsproject/Ranking.java
@@ -48,9 +48,9 @@ public class Ranking extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_ranking,container,false);
-
+                             Bundle slater.inflate(R.layout.fragment_ranking,container,false);
+    avedInstanceState) {
+        View rootView = inf
         try {
             listAPP = (ListView) rootView.findViewById(R.id.listaAPP);
             TextView correo = (TextView)getActivity().findViewById(R.id.txCorreo);
